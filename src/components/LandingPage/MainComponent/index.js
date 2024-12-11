b35baeb50c5fe23ff6279f3f54c9f67d802e990e
sourabh <sourabhsshekhargol@gmail.com>
2024-12-12 00:50:09 +0530

    first commit

---
+++ src/components/LandingPage/MainComponent/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Button from "../../Common/Button";
+import "./styles.css";
+import gradient from "../../../assets/gradient.png";
+import iphone from "../../../assets/iphone.png";
+import { motion } from "framer-motion";
+import { RWebShare } from "react-web-share";
+import { toast } from "react-toastify";
+
+function MainComponent() {
+  return (
+    <div className="main-flex">
+      <div className="info-landing">
+        <motion.h1
+          className="heading1"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          Web 3.0 Crypto
+        </motion.h1>
+        <motion.h1
+          className="heading2"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.75, duration: 1 }}
+        >
+          Track. Analyze
+        </motion.h1>
+        <motion.p
+          className="info-text"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1, duration: 1 }}
+        >
+          Your ultimate crypto tracker delivering real-time insights, trends, and analytics for smarter cryptocurrency decisions.{" "}
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1.25, duration: 0.75 }}
+        >
+          <a href="/dashboard">
+            <Button text={"Dashboard"} />
+          </a>
+          
+        </motion.div>
+      </div>
+      <div className="gradient-div">
+        <img src={gradient} className="gradient" />
+        <motion.img
+          src={iphone}
+          className="iphone"
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 2,
+            repeat: Infinity,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MainComponent;
